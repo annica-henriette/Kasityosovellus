22e3a5dff4481478e6390b8dee6bd6903ffe6401
Annica <annicah@LAPTOP-USHUE94H.localdomain>
2023-03-24 10:23:31 +0200

    added app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+@app.route("/")
+def index():
+    return "Heipparallaa!"
+
+@app.route("/page1")
+def page1():
+    return "Tämä on sivu 1"
+
+@app.route("/page2")
+def page2():
+    return "Tämä on sivu 2"@app.route("/")
+def index():
+    return "Heipparallaa!"
+
+@app.route("/page1")
+def page1():
+    return "Tämä on sivu 1"
+
+@app.route("/page2")
+def page2():
+    return "Tämä on sivu 2"

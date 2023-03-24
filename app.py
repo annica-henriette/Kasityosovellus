6ee9ceb301b6de02c3a49033e046c77dba0b8a09
Annica <annicah@LAPTOP-USHUE94H.localdomain>
2023-03-24 13:47:57 +0200

    sql

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 from flask import Flask
-from flask import render_template, request
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///annicah"
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    words = ["apina", "banaani", "cembalo"]
-    return render_template("index.html", message="Tervetuloa!", items=words)
+    result = db.session.execute("SELECT content FROM messages")
+    messages = result.fetchall()
+    return render_template("index.html", count=len(messages), messages=messages) 
 
-@app.route("/order")
-def order():
-    return render_template("order.html")
+@app.route("/new")
+def new():
+    return render_template("new.html")
 
-@app.route("/result", methods=["POST"])
-def result():
-    pizza = request.form["pizza"]
-    extras = request.form.getlist("extra")
-    message = request.form["message"]
-    return render_template("result.html", pizza=pizza,
-                                          extras=extras,
-                                          message=message)
+@app.route("/send", methods=["POST"])
+def send():
+    content = request.form["content"]
+    sql = "INSERT INTO messages (content) VALUES (:content)"
+    db.session.execute(sql, {"content":content})
+    db.session.commit()
+    return redirect("/")

b3dfd63e99c4656c621cfe5fab170aa0281ecc63
Annica <annica.helsingius@helsinki.fi>
2023-03-26 15:15:59 +0300

    app.py paivitys

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///annicah"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://annicahelsingius"
 db = SQLAlchemy(app)
 
 @app.route("/")

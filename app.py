f2c7035c2dac05fb632f9159512119b3d5b495f9
Annica <annica.helsingius@helsinki.fi>
2023-03-27 11:38:18 +0300

    updates

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from os import getenv
 from flask import Flask
-from flask import redirect, render_template, request
+from flask import redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 from sqlalchemy.sql import text
 
 app = Flask(__name__)
@@ -10,55 +10,8 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    sql = text("SELECT id, topic, created_at FROM polls ORDER BY id DESC")
-    result = db.session.execute(sql)
-    polls  = result.fetchall()
-    return render_template("index.html",polls=polls) 
-
-@app.route("/new")
-def new():
-    return render_template("new.html")
-
-@app.route("/create", methods=["POST"])
-def create():
-    topic = request.form["topic"]
-    sql = text("INSERT INTO polls (topic, created_at) VALUES (:topic, NOW()) RETURNING id")
-    result = db.session.execute(sql, {"topic":topic})
-    poll_id = result.fetchone()[0]
-    choices = request.form.getlist("choice")
-    for choice in choices:
-        if choice != "":
-            sql = text("INSERT INTO choices (poll_id, choice) VALUES (:poll_id, :choice)")
-            db.session.execute(sql, {"poll_id":poll_id, "choice":choice})
+    db.session.execute(text("INSERT INTO visitors (time) VALUES (NOW())"))
     db.session.commit()
-    return redirect("/")
-
-@app.route("/poll/<int:id>")
-def poll(id):
-    sql = text("SELECT topic FROM polls WHERE id=:id")
-    result = db.session.execute(sql, {"id":id})
-    topic = result.fetchone()[0]
-    sql = text("SELECT id, choice FROM choices WHERE poll_id=:id")
-    result = db.session.execute(sql, {"id":id})
-    choices = result.fetchall()
-    return render_template("poll.html", id=id, topic=topic, choices=choices)
-
-@app.route("/answer", methods=["POST"])
-def answer():
-    poll_id = request.form["id"]
-    if "answer" in request.form:
-        choice_id = request.form["answer"]
-        sql = text("INSERT INTO answers (choice_id, sent_at) VALUES (:choice_id, NOW())")
-        db.session.execute(sql, {"choice_id":choice_id})
-        db.session.commit()
-    return redirect("/result/" + str(poll_id))
-
-@app.route("/result/<int:id>")
-def result(id):
-    sql = text("SELECT topic FROM polls WHERE id=:id")
-    result = db.session.execute(sql, {"id":id})
-    topic = result.fetchone()[0]
-    sql = text("SELECT c.choice, COUNT(a.id) FROM choices c LEFT JOIN answers a ON c.id=a.choice_id WHERE c.poll_id=:poll_id GROUP BY c.id")
-    result = db.session.execute(sql, {"poll_id":id})
-    choices = result.fetchall()
-    return render_template("result.html", topic=topic, choices=choices)
+    result = db.session.execute(text("SELECT COUNT(*) FROM visitors"))
+    counter = result.fetchone()[0]
+    return render_template("index.html", counter=counter) 

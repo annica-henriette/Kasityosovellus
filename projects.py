964e36f86859e6085d88caa1a95b47158b550325
Annica <annica.helsingius@helsinki.fi>
2023-03-28 14:44:46 +0300

    added method

--- projects.py
+++ projects.py
@@ -7,10 +7,15 @@ def get_projects():
     result = db.session.execute(sql)
     return result.fetchall()
 
+def get_project_info(project_id):
+    sql = text("""SELECT p.name, p.material, p.start_date, p.finishing_date FROM projects p, users u
+                  WHERE p.id=:project_id AND p.creator_id=u.id""")
+    return db.session.execute(sql, {"project_id": project_id}).fetchone()
+
 def add_project(creator_id, name, material, start_date, finishin_date):
     sql = text("""INSERT INTO projects (creator_id, name, material, start_date, finishing_date)
                   VALUES (:creator_id, :name, :material, :start_date, :finishing_date)
                   RETURNING id""")
-    project_id = db.session.execute(sql, {"creator_id":creator_id}).fetchone()[0]
+    project_id = db.session.execute(sql, {"creator_id":creator_id}).fetchone()
     db.session.commit()
     return project_id

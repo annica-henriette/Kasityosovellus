72e38b7779f5e98873021a945ca3034b5e1d60b3
Annica <annica.helsingius@helsinki.fi>
2023-03-28 14:48:46 +0300

    error fixes

--- projects.py
+++ projects.py
@@ -16,6 +16,6 @@ def add_project(creator_id, name, material, start_date, finishin_date):
     sql = text("""INSERT INTO projects (creator_id, name, material, start_date, finishing_date)
                   VALUES (:creator_id, :name, :material, :start_date, :finishing_date)
                   RETURNING id""")
-    project_id = db.session.execute(sql, {"creator_id":creator_id}).fetchone()
+    project_id = db.session.execute(sql, {"creator_id":creator_id, "name":name}).fetchone()[0]
     db.session.commit()
     return project_id
